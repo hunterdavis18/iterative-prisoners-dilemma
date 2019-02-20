349aabf2a0fbe3631989af5a5feb75e2dd384412
Hunter Davis <hunter.davis@teachscs.org>
2019-02-20 19:47:07 +0000

    Test Check

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -380,3 +380,5 @@ def post_to_file(string, filename='tournament.txt', directory=''):
 if __name__ == '__main__':
     scores, moves, reports = main_play(modules[0:4])   
     section0, section1, section2, section3 = reports
+    #scores, moves, reports = main_play([exaple1, example2, team0])
+    #scores, moves, reports = main_play([example1]*3 + [example2]*2)
