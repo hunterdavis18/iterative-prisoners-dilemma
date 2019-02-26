718ba082e914ff814715ed7775da364856b1f9d2
Garrett Cherry <garrett.cherry@teachscs.org>
2019-02-26 19:47:11 +0000

    team9 Ready

--- team9.py
+++ team9.py
@@ -1,3 +1,5 @@
+import random
+
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -11,22 +13,18 @@ strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    G=random.randint(1,2)
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    if G==2:
+        return "c"
+      
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    
+       
+    if G==1:
+      return "b"
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -66,3 +64,6 @@ if __name__ == '__main__':
               my_score=0, 
               their_score=0,
               result='b')             
+              
+              
+
